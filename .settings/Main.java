a9c801e9cb20846f5d74993cc18c5bd67d01aad8
NIROhazan <156203893+NIROhazan@users.noreply.github.com>
2024-09-03 12:38:17 +0300

    Add files via upload

---
+++ .settings/Main.java
@@ -0,0 +1,16 @@
+//Final Project - Nir Hazan 316009489 & May Setter 312123037
+package LZW;
+
+
+ class Main {
+
+    /**
+     * The main method initializes the GUI and sets up the program.
+     *
+     * @param args Command-line arguments (not used in this application).
+     */
+    public static void main(String[] args) {
+        GUI guiRunner= new GUI();
+        guiRunner.startGUI(guiRunner);
+    }
+}
